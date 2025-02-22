41bf74f4fa4105fb2de28cbc5fb78b4ea6cf5561
Yanwen Jia <2058003243@qq.com>
2025-02-22 10:00:44 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# 创建Flask应用实例
 app = Flask(__name__)
 
+# 定义虚拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+# 定义路由和视图函数
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    # 渲染主页模板，传入name和movies变量
+    return render_template('index.html',name=name,movies=movies)
+
