0a60550eb9e1d2e81f6ffddf4c323611e6d1d6e3
Yanwen Jia <2058003243@qq.com>
2025-03-04 11:38:13 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -68,10 +68,19 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
-#在主页视图读取数据库记录
+#模板优化：模板上下文处理函数
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
 
